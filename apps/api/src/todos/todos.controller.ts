8c498e23d2666518e0820674ffb0f16dbbec5671
call5h5ong <call5h5ong@gmail.com>
2023-07-01 15:11:31 +0900

    todo 추가

--- apps/api/src/todos/todos.controller.ts
+++ apps/api/src/todos/todos.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AddTodoDto } from './dtos/add-todo.dto';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private todosService: TodosService) {}
+
+  @Post('add')
+  addTodo(@Body() addTodoDto: AddTodoDto) {
+    const { text } = addTodoDto;
+    this.todosService.add(text);
+  }
+}
--- apps/api/src/todos/todos.service.ts
+++ apps/api/src/todos/todos.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from './interfaces/todos.interface';
 
 @Injectable()
-export class TodosService {}
+export class TodosService {
+  private todosStore: Todo[] = [];
+
+  add(text: string) {
+    this.todosStore.push({ text: text, isDone: false });
+  }
+}
